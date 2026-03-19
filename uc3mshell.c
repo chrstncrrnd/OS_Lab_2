efd35e177ef252e40ae091adf40ff8cd9051d910
chrstncrrnd <chrstncrrnd@gmail.com>
2026-03-19 14:08:07 +0100

    started work on parsing line

--- uc3mshell.c
+++ uc3mshell.c
@@ -24,7 +24,9 @@
 // process structure (used for managing processes)
 typedef struct {
   pid_t pid;
-  char* name;
+  int argc;
+  // name is argv[0]
+  char** argv;
 } proc_t;
 
 
@@ -40,7 +42,7 @@ typedef struct {
 vec_proc* create_vec_proc(){
   vec_proc* out = malloc(sizeof(vec_proc));
   if (out == NULL){
-    fprintf(stderr,"Error allocating memory on heap!");
+    perror("ERROR: allocating memory for process vector struct!");
     free(out);
     _exit(-1);
   }
@@ -51,20 +53,20 @@ vec_proc* create_vec_proc(){
 
   // check that the memory was correctly allocated
   if (out->values == NULL){
-    fprintf(stderr,"Error allocating memory on heap!");
-    free(out->values);
+    perror("ERROR: allocating memory for process vector elements!");
     _exit(-1);
   }
   return out;
 }
 
+// append value to vector
 void append_vec_proc(vec_proc* vector, proc_t value){
   if (vector->size == vector->capacity){
     // reallocate vector
     vector->capacity = vector->capacity * 2;
     vector->values = realloc(vector->values, vector->capacity * sizeof(proc_t));
     if(vector->values == NULL){
-      fprintf(stderr, "Error reallocating memory on heap!");
+      perror("ERROR: reallocating memory for process vectors!");
       _exit(-1);
     }
   }
@@ -81,12 +83,19 @@ void destroy_vec_proc(vec_proc* to_destroy){
 }
 
 
-void process_line(char* line, int line_number){ 
+// once we have parsed the line into proc_t, we can proceed to execute them
+void exec_line(vec_proc* line){
+  line = (void*) line;
+}
+
+
+// line to vector of processes
+vec_proc* process_line(char* line, int line_number){ 
   strip(line);
 
   size_t line_len = strlen(line);
   if (line_len == 0){
-    return;
+    return NULL;
   }
 
 
@@ -94,30 +103,48 @@ void process_line(char* line, int line_number){
     if (strcmp(line, "## Uc3mshell P2") != 0){
       fprintf(stderr, "ERROR: Unexpected first line, got %s!\n", line);
       _exit(-1);
+    }else{
+      return NULL;
     }
   }
+  if (line[0] == '#'){
+    return NULL;
+  }
 
-  printf("Line: |%s|\n", line);
+
+  vec_proc* out = create_vec_proc();
+  char* word;
+  int words_read = 0;
+  proc_t current;
+  while ((word = strtok(words_read > 0 ? NULL : line, " ")) != NULL){
+    words_read ++;
+  }
+  return out;
 }
 
 
 void process_file(char* filename){
   int fd = open(filename, O_RDONLY);
   if (fd < 0){
-    fprintf(stderr, "ERROR: Couldn't open input file: %s.\n", filename);
+    perror("ERROR: Couldn't open input file");
     _exit(0);
   }
 
   char f_buf[F_RD_BUF_SIZE], line[max_line] = {0};
   int line_number = 0;
   ssize_t bytes_read, offset;
-
+  // read in chunks
   while ((bytes_read = read(fd, f_buf, F_RD_BUF_SIZE)) > 0){
     offset = -(ssize_t)strlen(line);
     for (ssize_t i = 0; i < bytes_read; i ++){
       if (f_buf[i] == '\n'){
         line[i - offset] = '\0';
-        process_line(line, line_number++);
+        // vector is freed once executed
+        vec_proc *to_exec = process_line(line, line_number++);
+        if (to_exec != NULL){
+          exec_line(to_exec);
+          destroy_vec_proc(to_exec);
+        }
         line[0] = '\0';
         offset = i + 1;
         continue;
